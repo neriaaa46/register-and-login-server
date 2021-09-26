1c3218661618913e1567737dd1e4c349bb5a0580
neriaaa46 <neriaaa46@gmail.comneria4ddfclear@idea-PC MINGW64 ~$ git config --global user.name neriaaa46@idea-PC MINGW64 ~$ git config --global user.email neriaaa46@gmail.com>
2021-09-27 00:45:37 +0300

    cookies

--- routes/csrfToken.js
+++ routes/csrfToken.js
@@ -1,11 +1,11 @@
 var express = require('express')
 var router = express.Router()
 var csrf = require('csurf')
-var csrfProtection = csrf({cookie: { httpOnly: true, sameSite: 'none', secure: 'true'}})
+var csrfProtection = csrf({cookie: { httpOnly: true, sameSite: 'none', secure: 'true', path:'/'}})
 
 router.route("/")
 .get(csrfProtection, function(req, res) {
-    res.cookie('XSRF-TOKEN', req.csrfToken(), {sameSite: 'none', secure: 'true'})
+    res.cookie('XSRF-TOKEN', req.csrfToken(), {sameSite: 'none', secure: 'true', , path:'/'})
     res.json({})
 })
 
