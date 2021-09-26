bdf57db749b145e6c541451441b00fb3c7107e0e
neriaaa46 <neriaaa46@gmail.comneria4ddfclear@idea-PC MINGW64 ~$ git config --global user.name neriaaa46@idea-PC MINGW64 ~$ git config --global user.email neriaaa46@gmail.com>
2021-09-26 16:01:48 +0300

    cookies

--- routes/refreshToken.js
+++ routes/refreshToken.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var router = express.Router()
 var csrf = require('csurf')
-var csrfProtection = csrf({cookie: { httpOnly: true , domain: "https://wizardly-hopper-f2bf68.netlify.app"}})
+var csrfProtection = csrf({cookie: { httpOnly: true }})
 const {getTokens} = require("../utils/tokens")
 
 
@@ -12,7 +12,7 @@ router.route('/')
         const jwtToken = req.headers['authorization'].replace('Bearer ', '')        
         const {accessToken, refreshToken} = await getTokens(jwtToken, req.cookies.refreshToken)
 
-        res.cookie("refreshToken", refreshToken, {httpOnly: true, domain: "https://wizardly-hopper-f2bf68.netlify.app"})
+        res.cookie("refreshToken", refreshToken, {httpOnly: true, domain: "wizardly-hopper-f2bf68.netlify.app"})
         res.status(200).json({accessToken})
 
     }catch(error){
