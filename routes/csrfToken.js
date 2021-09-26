c5c70265094b993c72579043af19e7d484898ea3
neriaaa46 <neriaaa46@gmail.comneria4ddfclear@idea-PC MINGW64 ~$ git config --global user.name neriaaa46@idea-PC MINGW64 ~$ git config --global user.email neriaaa46@gmail.com>
2021-09-26 13:13:44 +0300

    cookies

--- routes/csrfToken.js
+++ routes/csrfToken.js
@@ -1,11 +1,11 @@
 var express = require('express')
 var router = express.Router()
 var csrf = require('csurf')
-var csrfProtection = csrf({cookie: { httpOnly: true}})
+var csrfProtection = csrf({cookie: { httpOnly: true, domain: "https://wizardly-hopper-f2bf68.netlify.app"}})
 
 router.route("/")
 .get(csrfProtection, function(req, res) {
-    res.cookie('XSRF-TOKEN', req.csrfToken(), {domain: ".wizardly-hopper-f2bf68.netlify.app.com"})
+    res.cookie('XSRF-TOKEN', req.csrfToken(), {domain: "https://wizardly-hopper-f2bf68.netlify.app"})
     res.json({})
 })
 
