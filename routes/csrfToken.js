ea8ba8b5e3002ce35e09001ee98cffa1a5bcd273
neriaaa46 <neriaaa46@gmail.comneria4ddfclear@idea-PC MINGW64 ~$ git config --global user.name neriaaa46@idea-PC MINGW64 ~$ git config --global user.email neriaaa46@gmail.com>
2021-09-27 00:23:52 +0300

    cookies

--- routes/csrfToken.js
+++ routes/csrfToken.js
@@ -1,11 +1,11 @@
 var express = require('express')
 var router = express.Router()
 var csrf = require('csurf')
-var csrfProtection = csrf({cookie: { httpOnly: true, sameSite: 'none', secure: 'true'}})
+var csrfProtection = csrf({cookie: { httpOnly: true, domain: "https://register-and-login-app.netlify.app", sameSite: 'none', secure: 'true'}})
 
 router.route("/")
 .get(csrfProtection, function(req, res) {
-    res.cookie('XSRF-TOKEN', req.csrfToken(), {sameSite: 'none', secure: 'true'})
+    res.cookie('XSRF-TOKEN', req.csrfToken(), {domain: "https://register-and-login-app.netlify.app", sameSite: 'none', secure: 'true'})
     res.json({})
 })
 
--- routes/refreshToken.js
+++ routes/refreshToken.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var router = express.Router()
 var csrf = require('csurf')
-var csrfProtection = csrf({cookie: { httpOnly: true, sameSite: 'none', secure: 'ture' }})
+var csrfProtection = csrf({cookie: { httpOnly: true, domain: "https://register-and-login-app.netlify.app", sameSite: 'none', secure: 'ture' }})
 const {getTokens} = require("../utils/tokens")
 
 
@@ -12,7 +12,7 @@ router.route('/')
         const jwtToken = req.headers['authorization'].replace('Bearer ', '')        
         const {accessToken, refreshToken} = await getTokens(jwtToken, req.cookies.refreshToken)
 
-        res.cookie("refreshToken", refreshToken, {sameSite: 'none', secure: 'true'})
+        res.cookie("refreshToken", refreshToken, {domain: "https://register-and-login-app.netlify.app", sameSite: 'none', secure: 'true'})
         res.status(200).json({accessToken})
 
     }catch(error){
