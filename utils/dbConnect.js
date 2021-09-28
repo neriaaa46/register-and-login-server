1a687e313af391dc7c35a3ee548e4132c4c166e4
neriaaa46 <neriaaa46@gmail.comneria4ddfclear@idea-PC MINGW64 ~$ git config --global user.name neriaaa46@idea-PC MINGW64 ~$ git config --global user.email neriaaa46@gmail.com>
2021-09-28 23:46:29 +0300

    db

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -5,8 +5,8 @@ const db = mysql.createPool({
     connectionLimit: 10,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     database:  process.env.DB_NAME, 
-    password: process.env.DB_PASSWORD
   })
 
 
