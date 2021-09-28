5138fa599645b927889cbf5aec6e4e17c87e0dae
neriaaa46 <neriaaa46@gmail.comneria4ddfclear@idea-PC MINGW64 ~$ git config --global user.name neriaaa46@idea-PC MINGW64 ~$ git config --global user.email neriaaa46@gmail.com>
2021-09-28 23:50:45 +0300

    db

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -3,9 +3,9 @@ const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
     connectionLimit: 10,
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    database:  process.env.DB_NAME, 
+    host: 'eu-cdbr-west-01.cleardb.com',
+    user: ' ba93bc4c8b74eb',
+    database: 'heroku_8f772d7e14d0609', 
     password: 'cac7167c'
   })
 
